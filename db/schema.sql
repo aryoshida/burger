7d8f0a1b7e0433717a66536f01499f7813281e29
Andrea Yoshida <aryoshida9340@gmail.com>
2019-05-21 21:56:08 -0600

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,18 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE(
+-- CREATE TABLE(
+--     id INT AUTO_INCREMENT,
+--     burger_name VARCHAR(45) NOT NULL,
+--     devoured BIT DEFAULT 0,
+-- )
+
+
+
+-- USE j2ad2goeznjc6588;
+CREATE TABLE burgers(
     id INT AUTO_INCREMENT,
     burger_name VARCHAR(45) NOT NULL,
     devoured BIT DEFAULT 0,
+    primary key(id)
 )
